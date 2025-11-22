c9037f250a7ef0f319debe27d160c8eec238e11d
Kay <kay.kasemir@gmail.com>
2025-11-22 16:52:34 -0500

    Run SwerveBot

--- src/main/java/frc/Main.java
+++ src/main/java/frc/Main.java
@@ -13,7 +13,8 @@ public final class Main
   {
     // System.out.println("Hello, robot!");
     // RobotBase.startRobot(frc.robot.Robot::new);
-    RobotBase.startRobot(frc.led.LEDRingDemoRobot::new);
+    // RobotBase.startRobot(frc.led.LEDRingDemoRobot::new);
     // RobotBase.startRobot(frc.demo.MotorSparkMiniDemoRobot::new);
+    RobotBase.startRobot(frc.swervebot.SwerveBot::new);
   }
 }
