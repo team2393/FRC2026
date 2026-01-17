06a7c50d97b386de93feda8382bbbb28e1d0004c
Kay <kay.kasemir@gmail.com>
2026-01-17 13:52:08 -0500

    PowerDistribution, elasitc board

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,28 +5,39 @@ package frc.robot;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.tools.CommandRobotBase;
 import frc.camera.CameraHelper;
-import frc.camera.RotateToTarget;
 import frc.swervelib.RelativeSwerveCommand;
 
 /** FRC2026 robot */
 public class Robot extends CommandRobotBase
 {
+    /** XXX Pick k2026RebuiltWelded or k2026RebuiltAndymark */
     private final AprilTagFieldLayout tags = AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltWelded);
+
+    /** Track if hub is active */
+    private final HubTimer hub_timer = new HubTimer();
+
+    /** Power distribution board to monitor current */
+    private final PowerDistribution power_dist = new PowerDistribution();
+
+    /** Drivetrain and related commands */
     private final RobotDrivetrain drivetrain = new RobotDrivetrain();
     private final Command joydrive = new RelativeSwerveCommand(drivetrain);
     private final Command aim = new AimToHub(tags, drivetrain);
     // private final Command aim = new RotateToTarget("Front", drivetrain);
+
+    // private final FuelHandler fuel_handler = new FuelHandler();
+
+    /** Handle cameras */
     private final CameraHelper camera = new CameraHelper(tags, "Front", "FrontCamera",
                                                          0.34, -0.1, 0.16,
                                                          0.0,
                                                          -10.0);
-    // private final FuelHandler fuel_handler = new FuelHandler();
-
-    private final HubTimer hub_timer = new HubTimer();
 
     public Robot()
     {
@@ -34,24 +45,30 @@ public class Robot extends CommandRobotBase
         RobotOI.MAX_METERS_PER_SEC = 1.0;
         RobotOI.MAX_ROTATION_DEG_PER_SEC = 180.0;
 
+        // Bind controller buttons
         RobotOI.joystick.x().whileTrue(aim);
-
         // RobotOI.joystick.a().onTrue(fuel_handler.take_in());
         // RobotOI.joystick.y().onTrue(fuel_handler.shoot());
+
+        // Power dist. info
+        power_dist.clearStickyFaults();
+        SmartDashboard.putData("Power", power_dist);
     }
 
     @Override
     public void robotPeriodic()
     {
         super.robotPeriodic();
-
         camera.updatePosition(drivetrain);
     }
 
     @Override
     public void teleopInit()
     {
+        // Start tracking the hub state
         CommandScheduler.getInstance().schedule(hub_timer);
+
+        // By default, drive, and allow bound buttons to select other modes
         drivetrain.setDefaultCommand(joydrive);
     }
 }
