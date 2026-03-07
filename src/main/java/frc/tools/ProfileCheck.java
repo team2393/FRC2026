f3ae06f6f9c6860deecb4c5d4c22e1decafe533b
Kay <kay.kasemir@gmail.com>
2026-03-07 17:22:35 -0500

    ProfileCheck for moves that don't reach full speed

--- src/main/java/frc/tools/ProfileCheck.java
+++ src/main/java/frc/tools/ProfileCheck.java
@@ -3,9 +3,6 @@
 // the WPILib BSD license file in the root directory of this project.
 package frc.tools;
 
-import edu.wpi.first.units.measure.Distance;
-import edu.wpi.first.units.measure.Time;
-
 /** Check motion profile settings */
 public class ProfileCheck
 {
@@ -21,17 +18,26 @@ public class ProfileCheck
         // Time to accelerate, distance covered in that time
         double t1 = max_speed/accel;
         double d1 = 0.5*accel*t1*t1;
-        System.out.format("It will take %.1f s to accelerate, moving by %.1f\n", t1, d1);
 
         // Deceleration will be the same
         double t3 = t1;
         double d3 = d1;
-        if (d1+d3 > dist)
-            System.out.println("Can't accelerate to full speed, results will be off");
 
         // Remaining distance, covered at full speed
         double d2 = dist-d1-d3;
         double t2 = d2/max_speed;
+
+        if (d1+d3 > dist)
+        {
+            // Can't accelerate to full speed
+            // Accelerate half the distane, decellerate the other half
+            d1 = d3 = dist/2;
+            d2 = 0;
+            t1 = t3 = Math.sqrt(2*d1/accel);
+            t2 = 0;
+        }
+
+        System.out.format("It will take %.1f s to accelerate, moving by %.1f\n", t1, d1);
         System.out.format("We'll run for %.1f s at full speed, covering %.1f\n", t2, d2);
         System.out.format("It will take %.1f s to decellerate, moving by %.1f\n",  t3, d3);
 
