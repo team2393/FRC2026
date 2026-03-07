7eb9675f3b17879c7358f7eda423b6ddb5ea00b9
drivestation <ds@nowhere.com>
2026-03-07 12:48:03 -0500

    slower

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -82,7 +82,7 @@ public class Robot extends CommandRobotBase
         RobotOI.MAX_METERS_PER_SEC = 2.0;
         RobotOI.MAX_ROTATION_DEG_PER_SEC = 180.0;
         // Max speed used in auto
-        AutoTools.config = new TrajectoryConfig(2.5, 2.0);
+        AutoTools.config = new TrajectoryConfig(1.5, 2.0);
         AutoTools.config.addConstraint(new SwerveDriveKinematicsConstraint(drivetrain.getKinematics(),
                                                                            SwerveDrivetrain.MAX_METERS_PER_SEC));
 
