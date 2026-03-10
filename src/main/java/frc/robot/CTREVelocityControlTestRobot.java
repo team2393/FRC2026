081dc818456b3299c3423ea20028a3141ab70c29
Kay <kay.kasemir@gmail.com>
2026-03-10 19:37:36 -0400

    Prep for spinner test

--- src/main/java/frc/robot/CTREVelocityControlTestRobot.java
+++ src/main/java/frc/robot/CTREVelocityControlTestRobot.java
@@ -4,34 +4,44 @@
 package frc.robot;
 
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
 
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.tools.CommandRobotBase;
 
-/** Storage or Feeder test */
+/** Spinner test */
 public class CTREVelocityControlTestRobot extends CommandRobotBase
 {
-    private final static double UNITS_PER_TURN = 0.3/6.3;
+    private final static double UNITS_PER_TURN = 1.0;
 
-    private final TalonFX motor = MotorHelper.createTalonFX(2, false, false, 0, 0.0);
+    private final TalonFX motor = MotorHelper.createTalonFX(RobotMap.SPINNER, false, false, 0,40);
+    private final TalonFX motor2 = MotorHelper.createTalonFX(RobotMap.SPINNER2, false, false, 0, 40);
 
-    private NetworkTableEntry nt_setpoint = SmartDashboard.getEntry("Setpoint"),
-                              nt_velocity = SmartDashboard.getEntry("Velocity");
+
+    private NetworkTableEntry nt_setpoint  = SmartDashboard.getEntry("Setpoint"),
+                              nt_setpoint1 = SmartDashboard.getEntry("Setpoint1"),
+                              nt_setpoint2 = SmartDashboard.getEntry("Setpoint2"),
+                              nt_velocity  = SmartDashboard.getEntry("Velocity");
 
     public CTREVelocityControlTestRobot()
     {
+        motor2.setControl(new Follower(motor.getDeviceID(), MotorAlignmentValue.Opposed));
+
         var pid = new Slot0Configs();
         pid.kS = 0;
-        pid.kV = 0;
-        pid.kP = 0;
-        pid.kI = 0;
+        pid.kV = 0; // 0.00213
+        pid.kP = 0; // 0.005
+        pid.kI = 0; // 0.04
         pid.kD = 0;
         motor.getConfigurator().apply(pid);
 
         nt_setpoint.setDefaultDouble(0);
+        nt_setpoint1.setDefaultDouble(1800);
+        nt_setpoint2.setDefaultDouble(2250);
     }
 
     @Override
@@ -52,7 +62,11 @@ public class CTREVelocityControlTestRobot extends CommandRobotBase
     @Override
     public void autonomousPeriodic()
     {
-        request.withVelocity(nt_setpoint.getDouble(0) / UNITS_PER_TURN);
+        double setpoint = (System.currentTimeMillis() / 5000) % 2 == 0
+                        ? nt_setpoint1.getDouble(0)
+                        : nt_setpoint2.getDouble(0);
+        nt_setpoint.setDouble(setpoint);
+        request.withVelocity(setpoint / UNITS_PER_TURN);
         motor.setControl(request);
     }
 }
