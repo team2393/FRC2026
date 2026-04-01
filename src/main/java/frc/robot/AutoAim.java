aa68310b436eda51f3aa59ddcbe56ddfec80c5ca
drivestation <ds@nowhere.com>
2026-03-31 20:03:35 -0400

    New distance settings

--- src/main/java/frc/robot/AutoAim.java
+++ src/main/java/frc/robot/AutoAim.java
@@ -54,14 +54,14 @@ public class AutoAim extends SubsystemBase
     private final static LookupTable settings_table = new LookupTable(
         // distance, speed, hood, deviation
         //       [m], [rpm],  [%], deviation
-         1.0,  1800,   40,         0,
-                 1.5,  1900,   50,         0,
-                 2.0,  2000,   60,         0,
-                 2.5,  2100,   70,         0,
-                 3.0,  2125,   75,         0,
+         1.0,  1750,   35,         0,
+                 1.5,  1900,   40,         0,
+                 2.0,  2000,   50,         0,
+                 2.5,  2100,   55,         0,
+                 3.0,  2150,   70,         0,
                  3.5,  2150,   85,         0,
                  4.0,  2250,   90,         0,
-                 4.5,  2350,   90,         0,
+                 4.5,  2400,   90,         0,
                  5.0,  2400,   95,         0,
                  7.0,  2400,   95,         0,
                  8.0,  2500,   95,         0);
